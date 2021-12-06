0a84c829f746504258ff83343d6f3fb16186ad8f
Shotgn16 <jack.tm68@googlemail.com>
2021-12-06 13:09:48 +0000

    v0.06

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serviceRestart
+{
+    internal class Program
+    {
+        public static void restartService(string serviceName, int timeOutMS)
+        {
+            ServiceController service = new ServiceController(serviceName);
+
+            TimeSpan timeout = TimeSpan.FromMilliseconds(timeOutMS);
+
+            try
+            {
+                //If service is stopped...
+                if (service.Status == ServiceControllerStatus.Stopped)
+                {
+                    service.Start();
+
+                    service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+
+                    MessageBox.Show($"The service '{serviceName}', has been successfully started!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                //If service is Running...
+                else if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+
+                    MessageBox.Show($"The service '{serviceName}', has been successfully restarted!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                restartService("MyQ_Sharp", 30000);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
